ec1455e40111f7767e3389809f0ff85687011541
kamva-hanisi <lucashanisi@gmail.com>
2024-06-14 00:00:02 +0000

    SQL

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,7 @@
 -- a SQL script that lists all bands with Glam rock as their main style
 -- import mySQL
-SELECT band_name, IFNULL(split, 2021) - formed AS lifespan
+
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET("Glam rock", style)
 ORDER BY lifespan DESC;
